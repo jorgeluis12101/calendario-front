027c8df3c42c03ee55df7e926cbf03848c46be65
jorgeluis12101 <jcastillov12@upao.edu.pe>
2024-05-12 22:08:32 -0500

    pepe

--- src/app/Compartido/componentes/user/eventos-tardios/eventos-tardios.component.ts
+++ src/app/Compartido/componentes/user/eventos-tardios/eventos-tardios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../../../service/event.service';
 import { Evento } from '../../../evento';  // Asegúrate de que la ruta es correcta
+import { AuthService } from './../../../../service/auth.service';
 
 @Component({
   selector: 'app-eventos-tardios',
@@ -10,19 +11,24 @@ import { Evento } from '../../../evento';  // Asegúrate de que la ruta es corre
 export class EventosTardiosComponent implements OnInit {
   eventos: Evento[] = [];
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private authService: AuthService) {}
 
   ngOnInit() {
       this.loadEventosActivos();
   }
 
   loadEventosActivos() {
-      const usuarioId = 1; // Debe obtenerse de manera segura
-      this.eventService.getEventosActivosByUsuario(usuarioId).subscribe({
-          next: (eventos) => this.eventos = eventos,
-          error: (error) => console.error('Error al obtener eventos activos:', error)
-      });
+    const userId = this.authService.getUsuarioId(); // Asume que existe este métod
+    if (userId) {
+      this.eventService.getEventosActivosByUsuario(userId).subscribe({
+        next: (eventos) => this.eventos = eventos,
+        error: (error) => console.error('Error al obtener eventos activos:', error)
+    });
+    }
+      
   }
+
+  
   
   completarEvento(id: number): void {
     this.eventService.completarEvento(id).subscribe({
